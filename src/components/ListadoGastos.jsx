8e002da334c1d886fb7b0d5050e1797fa816187f
trip4losky <agustinrallysport@gmail.com>
2023-09-06 13:03:30 +0100

    Functionality improvements

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Gasto from './Gasto'
 
-const ListadoGastos = ({ gastos, setGastoEditar }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, borrarGasto }) => {
     return (
         <div className='listado-gastos contenedor'>
             <h2>{gastos.lenght ? 'Gastos' : 'No Hay Gastos aún'}</h2>
@@ -10,6 +10,7 @@ const ListadoGastos = ({ gastos, setGastoEditar }) => {
                     key={gasto.id}
                     gasto={gasto}
                     setGastoEditar={setGastoEditar}
+                    borrarGasto={borrarGasto}
                 />
             ))}
         </div>
