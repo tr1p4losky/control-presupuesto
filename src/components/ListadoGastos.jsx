dfdd4e6d6e60e2a2a3478899f46bd0670d5af337
trip4losky <agustinrallysport@gmail.com>
2023-09-07 12:17:16 +0100

    Adds graphics, filters and new features

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import Gasto from './Gasto'
 
-const ListadoGastos = ({ gastos, setGastoEditar, borrarGasto }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, borrarGasto, filtro, gastosFiltrados }) => {
     return (
         <div className='listado-gastos contenedor'>
-            <h2>{gastos.lenght ? 'Gastos' : 'No Hay Gastos aún'}</h2>
-            {gastos.map(gasto => (
-                <Gasto
-                    key={gasto.id}
-                    gasto={gasto}
-                    setGastoEditar={setGastoEditar}
-                    borrarGasto={borrarGasto}
-                />
-            ))}
+            {
+                filtro ? (
+                    <>
+                        <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay Gastos en esta categoría'}</h2>
+                        {gastosFiltrados.map(gasto => (
+                            <Gasto
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                                borrarGasto={borrarGasto}
+                            />
+                        ))}
+
+                    </>
+
+                ) : (
+                    <>
+                        <h2>{gastos.length ? 'Gastos' : 'No hay Gastos aún'}</h2>
+                        {gastos.map(gasto => (
+                            <Gasto
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                                borrarGasto={borrarGasto}
+                            />
+                        ))}
+                    </>
+                )
+            }
         </div>
     )
 }
